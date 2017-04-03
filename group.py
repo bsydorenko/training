4fd5ea0fde5d0c3bd65cb700fa48970bac1671ba
iamtw1st <b.sydorenko@gmail.com>
2017-04-03 20:34:31 +0300

    rewriting tests for chrome

--- group.py
+++
@@ -1,5 +0,0 @@
-class Group:
-    def __init__(self, name, middle, lastname):
-        self.name = name
-        self.middle = middle
-        self.lastname = lastname
---
+++ test_add_group.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from selenium.webdriver.chrome.webdriver import WebDriver
+import unittest
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
+
+class test_add_group(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver('/Users/tw1st/Documents/aqapython/training/env/selenium/webdriver/chrome/webdriver')
+        self.wd.implicitly_wait(60)
+
+    def test_test_add_group(self):
+        success = True
+        wd = self.wd
+        wd.get("http://localhost:3000/addressbook/index.php")
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        wd.find_element_by_xpath("//div[@id='footer']//li[.='php-addressbook v8.2.5.2']").click()
+        wd.find_element_by_link_text("groups").click()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("new_group")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("new_group1")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("new_group1")
+        wd.find_element_by_name("submit").click()
+        wd.find_element_by_link_text("group page").click()
+        wd.find_element_by_link_text("Logout").click()
+        self.assertTrue(success)
+
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == '__main__':
+    unittest.main()
